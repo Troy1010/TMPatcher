e23a39f1763ff8bdfa506652f37b19a3dbe0bfd3
Troy <troyswanson1010@gmail.com>
2024-09-14 18:36:57 -0500

    start

---
+++ TMPatcher/Settings.cs
@@ -0,0 +1,12 @@
+﻿using Mutagen.Bethesda.WPF.Reflection.Attributes;
+
+namespace TMPatcher;
+
+public class Settings
+{
+    [MaintainOrder]
+
+    [SettingName("FeatureGuaranteeOneIngredientEffect")]
+    [Tooltip("Some UI mods with CTD if you open alchemy menu while an ingredient has 0 effects. This makes sure that doesn't occur.")]
+    public bool FeatureGuaranteeOneIngredientEffect = true;
+}
